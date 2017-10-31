4fddd5286e4f3e3cd0bc20ce08781df5f151f669
LeoThalman <lthalman15@wou.edu>
2017-10-31 14:23:02 -0700

    Added new page to load temp in

--- HW4/Controllers/HomeController.cs
+++ HW4/Controllers/HomeController.cs
@@ -16,35 +16,38 @@ namespace HW4.Controllers
             return View();
         }
 
+
         public ActionResult Page1()
         {
+            return View();
+        }
+
+        public ActionResult Page1Text()
+        {
+        if (Request.Form["temp"] != null && Request.Form["tempType"] != null)
+        {
+            string temp = Request.Form["temp"];
+            string tempType = Request.Form["tempType"];
             ViewBag.Title = "Page 1";
-            if (Request.Form["temp"] != null || Request.Form["tempType"] != null)
+            tempType = tempType.ToLower();
+            if (Double.TryParse(temp, out double nTemp))
             {
-                string temp = Request.Form["temp"];
-                string tempType = Request.Form["tempType"];
-                tempType = tempType.ToLower();
-                if (Double.TryParse(temp, out double nTemp))
+                if (tempType.Equals("c"))
                 {
-                    if (tempType.Equals("c"))
-                    {
-                        nTemp = (nTemp - 32) / 1.8;
-                        nTemp = Math.Round(nTemp, 2);
-                        ViewBag.nTemp = nTemp;
-
-                    }
-                    if (tempType.Equals("f"))
-                    {
-                        nTemp = (nTemp * 1.8) + 32;
-                        nTemp = Math.Round(nTemp, 2);
-                        ViewBag.nTemp = nTemp;
-                    }
-
-
+                    nTemp = (nTemp - 32) / 1.8;
+                    nTemp = Math.Round(nTemp, 2);
+                    ViewBag.nTemp = nTemp;
 
                 }
+                if (tempType.Equals("f"))
+                {
+                    nTemp = (nTemp * 1.8) + 32;
+                    nTemp = Math.Round(nTemp, 2);
+                    ViewBag.nTemp = nTemp;
+                }
             }
-            return View();
+        }
+        return View();
         }
     }
 }
