11b5f5ee5890750d7ecd4fdf0f2f999439d3a5fd
LeoThalman <lthalman15@wou.edu>
2017-10-31 13:20:57 -0700

    Added Temp conversion formula

--- HW4/Controllers/HomeController.cs
+++ HW4/Controllers/HomeController.cs
@@ -18,10 +18,32 @@ namespace HW4.Controllers
 
         public ActionResult Page1()
         {
-            string temp = Request.Form["temp"];
-            string tempType = Request.Form["tempType"];
+            ViewBag.Title = "Page 1";
+            if (Request.Form["temp"] != null || Request.Form["tempType"] != null)
+            {
+                string temp = Request.Form["temp"];
+                string tempType = Request.Form["tempType"];
+                tempType = tempType.ToLower();
+                if (Double.TryParse(temp, out double nTemp))
+                {
+                    if (tempType.Equals("c"))
+                    {
+                        nTemp = (nTemp - 32) / 1.8;
+                        nTemp = Math.Round(nTemp, 2);
+                        ViewBag.nTemp = nTemp;
 
-            Debug.WriteLine($"{temp} and {tempType}");
+                    }
+                    if (tempType.Equals("f"))
+                    {
+                        nTemp = (nTemp * 1.8) + 32;
+                        nTemp = Math.Round(nTemp, 2);
+                        ViewBag.nTemp = nTemp;
+                    }
+
+
+
+                }
+            }
             return View();
         }
     }
