8193b8b35332c822e9480641d6e78d91d3d679b9
LeoThalman <lthalman15@wou.edu>
2017-11-04 14:51:43 -0700

    Added comments and error handling

--- HW4/Controllers/HomeController.cs
+++ HW4/Controllers/HomeController.cs
@@ -68,6 +68,10 @@ namespace HW4.Controllers
         return View();
         }
 
+        /// <summary>
+        /// Returns the base page for Time conversion
+        /// </summary>
+        /// <returns>The base page for Time conversion</returns>
         [HttpGet]
         public ActionResult Page2()
         {
@@ -75,6 +79,15 @@ namespace HW4.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Takes the form from Page2 and pulls out the time and timeType fields
+        /// checks if time is a double and if time type is H or M, if true
+        /// on both accounts then converts time from hours to minutes or vice versa.
+        /// if false then returns an error message asking for a value for time
+        /// or H or M.
+        /// </summary>
+        /// <param name="form">the form containing time and timeType</param>
+        /// <returns>the view with an error message or the converted time</returns>
         [HttpPost]
         public ActionResult Page2(FormCollection form)
         {
@@ -108,24 +121,72 @@ namespace HW4.Controllers
             return View();
         }
 
-
+        /// <summary>
+        /// Returns the base page for Loan Calculator
+        /// </summary>
+        /// <returns>base page for Loan Calculator</returns>
         [HttpGet]
         public ActionResult Page3()
         {
+            ViewBag.Title = "Loan Calculator";
             return View();
         }
 
+        /// <summary>
+        /// Takes the principal payment, interest rate, and term length
+        /// and then checks if they're null, if so returns an error message.
+        /// Else it takes the three parameters and then uses a loan rate formula to
+        /// figure out the monthly payments. Sends the monthly payments to the view and also
+        /// fiugres out the total payment and sends that to the view.
+        /// </summary>
+        /// <param name="principal">principal payment</param>
+        /// <param name="interest">yearly interest rate</param>
+        /// <param name="termLength">term length in months</param>
+        /// <returns>The view with the error message or payments</returns>
         [HttpPost]
-        public ActionResult Page3(int? prin, double? iRate, double? numP)
+        public ActionResult Page3(double? principal, double? interest, double? termLength)
         {
             ViewBag.Title = "Loan Calculator";
-            iRate = iRate * .01;
-            iRate = iRate / 12;
-            double? payment = 0;
-            payment = (iRate * prin) / (1 - Math.Pow((1 + (double)iRate), -(double)numP));
-            payment = Math.Round((double)payment, 2);
-            ViewBag.mPay = "Your monthly payment is " + payment.ToString(); ;
+            ViewBag.errMessage = null;
+            if (LoanCheck(principal, interest, termLength))
+            {
+                ViewBag.errMessage = "Please enter a value in all 3 fields";
+            }
+            else
+            {
+                double prin = (double)principal;
+                double iRate = (double)interest;
+                double numP = (double)termLength;
+                iRate = iRate * .01;
+                iRate = iRate / 12;
+                double payment = 0;
+                payment = (iRate * prin) / (1 - Math.Pow((1 + iRate), -numP));
+                double total = payment * numP;
+                total = Math.Round(total, 2);
+                payment = Math.Round(payment, 2);
+                ViewBag.mPay = "Your monthly payment is " + payment.ToString();
+                ViewBag.tPay = "Your total payment is " + total.ToString();
+            }
             return View();
+        }
+
+        /// <summary>
+        /// Error checking for loan calculator, takes 3 parameters from
+        /// page 3 and checks to see if any are null, if so returns true else
+        /// returns false
+        /// </summary>
+        /// <param name="principal">principal payment</param>
+        /// <param name="interest">interest rate</param>
+        /// <param name="termLength">term length</param>
+        /// <returns>true if any value is null, else false</returns>
+        private Boolean LoanCheck(double? principal, double? interest, double? termLength)
+        {
+            Boolean rtn = false;
+            if(principal == null || interest == null || termLength == null)
+            {
+                rtn = true;
+            }
+            return rtn;
         }
     }
 }
